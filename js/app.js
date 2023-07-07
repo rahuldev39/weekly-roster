6a4aad28da8694ffe18f5a2692a911b1cacb3f33
rahuldev39 <rahuldev.w@gmail.com>
2023-07-07 11:34:32 +0530

    user and weekend added on top

--- js/app.js
+++
@@ -1,5 +0,0 @@
-const navbar = document.querySelector('.navbar');
-
-window.addEventListener('scroll',()=>{
-    (window.pageYOffset > 20) ? navbar.classList.add('bg-primary') : navbar.classList.remove('bg-primary')
-})
